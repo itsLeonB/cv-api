832f7c6bab9e7576a6ad81275f7d761bcc7359a5
itsLeonB <ellionblessan@gmail.com>
2024-10-26 20:09:59 +0700

    feat: update query numeric for optional query param

--- internal/delivery/apphttp/helper.go
+++ internal/delivery/apphttp/helper.go
@@ -1,17 +1,33 @@
 package apphttp
 
 import (
+	"fmt"
 	"strconv"
 
 	"github.com/gin-gonic/gin"
+	"github.com/itsLeonB/cv-api/internal/apperror"
+	"github.com/itsLeonB/cv-api/internal/delivery/apphttp/httperror"
 )
 
-func QueryNumeric(ctx *gin.Context, key string) (int, error) {
+func QueryNumeric(ctx *gin.Context, key string, isRequired bool) (int, bool, error) {
+	methodName := fmt.Sprintf("QueryNumeric(key: %s)", key)
 	param := ctx.Query(key)
-	val, err := strconv.Atoi(param)
-	if err != nil {
-		return 0, err
+	if param == "" {
+		if isRequired {
+			return 0, false, apperror.NewAppError(
+				httperror.BadRequestError(fmt.Sprintf("query param %s is required", key)),
+				"", methodName, "param == \"\" && isRequired",
+			)
+		}
+		return 0, false, nil
 	}
 
-	return val, nil
+	val, err := strconv.Atoi(param)
+	if err != nil {
+		return 0, false, apperror.NewAppError(
+			err, "", methodName, fmt.Sprintf("strconv.Atoi(param: %s)", param),
+		)
+	}
+
+	return val, true, nil
 }
